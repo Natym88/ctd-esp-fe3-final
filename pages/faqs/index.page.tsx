b9eb6e41def7b27a2dadc66e9ed41194d5a1cc2e
Naty <nataliajmoreira@gmail.com>
2024-03-24 14:17:51 -0300

    Probando faqs static page con endpoint de produccion

--- pages/faqs/index.page.tsx
+++ pages/faqs/index.page.tsx
@@ -1,5 +1,5 @@
-// import LayoutGeneral from "dh-marvel/components/layouts/layout-general";
-// import { BASE_URL } from "dh-marvel/services/APIConfig";
+import LayoutGeneral from "dh-marvel/components/layouts/layout-general";
+import { BASE_URL } from "dh-marvel/services/APIConfig";
 import { FaqsType } from "model/faq";
 import { NextPage, GetStaticProps } from "next";
 
@@ -8,37 +8,36 @@ interface FaqsPageProps {
 }
 
  const FaqsPage: NextPage<FaqsPageProps> | null = ({faqs}) => {
-	return null
- }
-// 	return (
-// 		<LayoutGeneral>
-// 			<section>
-// 				<h1 className="text-3xl font-bold my-6">FAQs</h1>
-// 				<div className="grid grid-cols-1 gap-4">
-// 					{faqs.map((faq) => (
-// 						<div
-// 							key={faq.id}
-// 							className="bg-slate-950/45 rounded-lg shadow-md p-4"
-// 						>
-// 							<h2 className="text-lg font-semibold mb-2">{faq.question}</h2>
-// 							<p className="text-gray-300">{faq.answer}</p>
-// 						</div>
-// 					))}
-// 				</div>
-// 			</section>
-// 		</LayoutGeneral>
-// 	);
-// };
 
-// export const getStaticProps: GetStaticProps = async (ctx) => {
-// 	const response = await fetch(BASE_URL + "api/faqs");
-// 	const faqs = await response.json();
+	return (
+		<LayoutGeneral>
+			<section>
+				<h1 className="text-3xl font-bold my-6">FAQs</h1>
+				<div className="grid grid-cols-1 gap-4">
+					{faqs.map((faq) => (
+						<div
+							key={faq.id}
+							className="bg-slate-950/45 rounded-lg shadow-md p-4"
+						>
+							<h2 className="text-lg font-semibold mb-2">{faq.question}</h2>
+							<p className="text-gray-300">{faq.answer}</p>
+						</div>
+					))}
+				</div>
+			</section>
+		</LayoutGeneral>
+	);
+};
 
-// 	return {
-// 		props: {
-// 			faqs,
-// 		},
-// 	};
-// };
+export const getStaticProps: GetStaticProps = async (ctx) => {
+	const response = await fetch(BASE_URL + "api/faqs");
+	const faqs = await response.json();
+
+	return {
+		props: {
+			faqs,
+		},
+	};
+};
 
  export default FaqsPage;
--- services/APIConfig/index.ts
+++ services/APIConfig/index.ts
@@ -1,4 +1,4 @@
 // Develop endpoint
-export const BASE_URL = "http://localhost:3000/";
+// export const BASE_URL = "http://localhost:3000/";
 // Production endpoint
-// export const BASE_URL = "";
+export const BASE_URL = "https://ctd-esp-fe3-final-three-liart.vercel.app/";
