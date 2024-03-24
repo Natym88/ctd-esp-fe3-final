86bc78f7e0d150164a0948ddef5a6ed125380d7e
Naty <nataliajmoreira@gmail.com>
2024-03-24 16:11:24 -0300

    creando nuevamente la ruta statica para la api de faqs

--- pages/faqs/index.page.tsx
+++ pages/faqs/index.page.tsx
@@ -1,4 +1,4 @@
-import LayoutGeneral from "dh-marvel/components/layouts/layout-general";
+import BodySingle from "dh-marvel/components/layouts/body/single/body-single";
 import { BASE_URL } from "dh-marvel/services/APIConfig";
 import { FaqsType } from "model/faq";
 import { NextPage, GetStaticProps } from "next";
@@ -8,36 +8,36 @@ interface FaqsPageProps {
 }
 
  const FaqsPage: NextPage<FaqsPageProps> | null = ({faqs}) => {
+	return null}
+// 	return (
+// 		<BodySingle>
+// 			<section>
+// 				<h1 className="text-3xl font-bold my-6">FAQs</h1>
+// 				<div className="grid grid-cols-1 gap-4">
+// 					{faqs.map((faq) => (
+// 						<div
+// 							key={faq.id}
+// 							className="bg-slate-950/45 rounded-lg shadow-md p-4"
+// 						>
+// 							<h2 className="text-lg font-semibold mb-2">{faq.question}</h2>
+// 							<p className="text-gray-300">{faq.answer}</p>
+// 						</div>
+// 					))}
+// 				</div>
+// 			</section>
+// 		</BodySingle>
+// 	);
+// };
 
-	return (
-		<LayoutGeneral>
-			<section>
-				<h1 className="text-3xl font-bold my-6">FAQs</h1>
-				<div className="grid grid-cols-1 gap-4">
-					{faqs.map((faq) => (
-						<div
-							key={faq.id}
-							className="bg-slate-950/45 rounded-lg shadow-md p-4"
-						>
-							<h2 className="text-lg font-semibold mb-2">{faq.question}</h2>
-							<p className="text-gray-300">{faq.answer}</p>
-						</div>
-					))}
-				</div>
-			</section>
-		</LayoutGeneral>
-	);
-};
+// export const getStaticProps: GetStaticProps = async () => {
+// 	const response = await fetch(BASE_URL + "api/faqs");
+// 	const faqs = await response.json();
 
-export const getStaticProps: GetStaticProps = async () => {
-	const response = await fetch(BASE_URL + "api/faqs");
-	const faqs = await response.json();
-
-	return {
-		props: {
-			faqs: Array.isArray(faqs) ? faqs : [],
-		},
-	};
-};
+// 	return {
+// 		props: {
+// 			faqs: Array.isArray(faqs) ? faqs : [],
+// 		},
+// 	};
+// };
 
  export default FaqsPage;
